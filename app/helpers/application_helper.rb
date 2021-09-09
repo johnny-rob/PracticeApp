36017ca4214fbb0e651d6f24ecce2e052e17846c
johnny-rob <jpinney1@gmail.com>
2021-09-09 08:00:17 -0400

    Set up devise links for logging in and out on application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+
+  def login_helper style = ''
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style)+
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
+  end
+
+
+  ## The next two def sections work together to create the nav items
+    def nav_items
+      [
+        {
+          url: root_path,
+          title: 'Home'
+        },
+        {
+          url: blogs_path,
+          title: 'About Me'
+        },
+      ]
+    end
+
+    def nav_helper style, tag_type
+      nav_links = ''
+      nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      end
+
+      nav_links.html_safe
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, path: '', path_names: { sign_in:'login', sign_out:'logout', sign_up:'register'}
   root to: 'pages#interiors'
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
